36d85493fe71d2aff07721ccd1e5cbb459295f39
Weerasak Chongnguluam <singpor@gmail.com>
2025-08-24 11:28:06 +0700

    Test send SMS API with httptest.Server

--- sms/sms_test.go
+++ sms/sms_test.go
@@ -2,14 +2,33 @@ package sms
 
 import (
 	"context"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func smsTestServer() *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		w.Write([]byte(`{
+			"id": 1,
+			"from": "TestSender",
+			"to": "+1234567890",
+			"text": "Hello, this is a test message.",
+			"status": "ACCEPTED"
+		}`))
+	}))
+}
+
 func TestSMSSender_SendSMS_Success(t *testing.T) {
+	server := smsTestServer()
+	defer server.Close()
+
 	smsSender := NewSMSSender(Config{
-		Endpoint:  "https://api.example.com",
+		Endpoint:  server.URL,
 		APIKey:    "test-api",
 		APISecret: "test-secret",
 	})
